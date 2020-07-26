3ff90d4fbd2f6c117e676261666a43469fe4f475
KleprlikJan <kleprlikjan@gmail.com>
2020-07-26 20:32:11 +0200

    Add project files.

---
+++ Yoti/Yoti.Droid/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content.PM;
+using Android.Views;
+
+namespace Yoti.Droid
+{
+	[Activity(
+			MainLauncher = true,
+			ConfigurationChanges = Uno.UI.ActivityHelper.AllConfigChanges,
+			WindowSoftInputMode = SoftInput.AdjustPan | SoftInput.StateHidden
+		)]
+	public class MainActivity : Windows.UI.Xaml.ApplicationActivity
+	{
+	}
+}
+
