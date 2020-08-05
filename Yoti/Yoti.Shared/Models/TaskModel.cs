b0840c3de305e081ea78a887846de2a00a00a88a
KleprlikJan <kleprlikjan@gmail.com>
2020-08-05 10:49:37 +0200

    Add Task/Tag models

---
+++ Yoti/Yoti.Shared/Models/TaskModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yoti.Shared.Models
+{
+	class Task
+	{
+		// Regular properties
+
+		public string Name { get; set; }
+		public short Difficulty { get; set; }
+		public TimeSpan Time {get; set;}
+		public DateTime Deadline { get; set; }
+		public Tag Tag { get; set; }
+		public string Description { get; set; }
+		public List<Task> Dependencies = new List<Task>();
+		public bool IsSeparable { get; set; }
+
+		// Properties for automatically generated tasks
+
+		public TimeSpan Period { get; set; }
+		public string Email { get; set; }   // TODO: May be replaced with more sophisticated type
+		public string DefaultEmailText { get; set; }
+
+	}
+}
---
+++ Yoti/Yoti.Shared/Models/TaskTags.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Yoti.Shared.Models
+{
+	struct Tag
+	{
+		public Tag(string name, Color color)
+		{
+			Name = name;
+			Color = color;
+		}
+
+		public string Name { get; }
+		public Color Color { get; }
+
+	}
+}
