11b7e9ba10699542a854eefbc656e987a0de1590
KleprlikJan <kleprlikjan@gmail.com>
2020-08-07 12:45:17 +0200

    Update MainPage Tag layout

--- Yoti/Yoti.Shared/ViewModels/MainPageVM.cs
+++ Yoti/Yoti.Shared/ViewModels/MainPageVM.cs
@@ -1,19 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
 using System.Text;
 using System.Windows.Input;
+using Uno.Extensions;
 using Windows.UI.Xaml.Controls;
+using Yoti.Shared.Models;
 
 namespace Yoti.Shared.ViewModels
 {
 	[Windows.UI.Xaml.Data.Bindable]
-	class MainPageVM 
+	public class MainPageVM 
 	{
+
+		public List<Tag> Tags = new List<Tag>
+		{
+			new Tag("All", Color.FromName("Blue")),
+			new Tag("All", Color.FromName("Green")),
+			new Tag("All", Color.FromName("Purple")),
+			new Tag("All", Color.FromName("Red")),
+			new Tag("All", Color.FromName("Cayan")),
+			new Tag("All", Color.FromName("Blue")),
+			new Tag("All", Color.FromName("Orange")),
+			new Tag("All", Color.FromName("Gray")),
+			new Tag("All", Color.FromName("Brown")),
+
+		};
+
+
 		public MainPageVM()
 		{
-			
+			Tasks.Add(new Task { Name = "name of the task 1" });
+			Tasks.Add(new Task { Name = "name of the task 2" });
+			Tasks.Add(new Task { Name = "name of the task 3" });
+			Tasks.Add(new Task { Name = "name of the task 4" });
 
 		}
+
+		public ObservableCollection<Task> Tasks = new ObservableCollection<Task>();
 
 	}
 }
--- Yoti/Yoti.Shared/Views/MainPage.xaml.cs
+++ Yoti/Yoti.Shared/Views/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,6 +13,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Yoti.Shared.Models;
+using Yoti.Shared.ViewModels;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +28,20 @@ namespace Yoti.Shared.Views
 		public MainPage()
 		{
 			this.InitializeComponent();
+			var MPVM = new MainPageVM();
+
+			foreach(Tag tag in MPVM.Tags)
+			{
+				ToggleButton b = new ToggleButton();
+				b.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(tag.Color.A, tag.Color.R, tag.Color.G, tag.Color.B));
+				b.Content = tag.Name;
+				TagButtonsPanel.Children.Add(b);
+			}
+		}
+
+		public void OnBack(object sender, RoutedEventArgs e)
+		{
+			this.Frame.Navigate(typeof(LoginPage));
 		}
 	}
 }
