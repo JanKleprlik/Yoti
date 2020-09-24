d0c8e86a6293d3155a127e51fa4b1bf8797e7380
KleprlikJan <kleprlikjan@gmail.com>
2020-09-24 19:55:55 +0200

    Fix TaskOverview control (All platforms working)

---
+++ Yoti/Yoti.Shared/Converters/ColorToStringConvertor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+
+namespace Yoti.Shared.Converters
+{
+	public class ColorToStringConvertor : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if (!(value is Color)) return value;
+			return value.ToString();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
