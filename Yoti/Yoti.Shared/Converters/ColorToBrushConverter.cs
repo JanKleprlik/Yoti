746ed7f2b348311eb5cc5d237b7117d23aff42a8
KleprlikJan <kleprlikjan@gmail.com>
2020-09-27 10:41:47 +0200

    Add ColorToBrushConverter

---
+++ Yoti/Yoti.Shared/Converters/ColorToBrushConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace Yoti.Shared.Converters
+{
+	public class ColorToBrushConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if (!(value is Color)) return value;
+			return new SolidColorBrush((Color)value);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
