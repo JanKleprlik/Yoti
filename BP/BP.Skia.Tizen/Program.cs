f2b3b9540c7743e0dc18c10a899958c1442105a1
KleprlikJan <kleprlikjan@gmail.com>
2021-02-10 18:50:54 +0100

    Add project files.

---
+++ BP/BP.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace BP.Skia.Tizen
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var host = new TizenHost(() => new BP.App(), args);
+			host.Run();
+		}
+	}
+}
---
+++ BP/BP.Skia.Wpf/Program.cs
@@ -0,0 +1,4 @@
+﻿
+namespace BP.Skia.Gtk
+{
+}
