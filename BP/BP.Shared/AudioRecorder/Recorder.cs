f230ab68cc31fd62a439a9918ab4b635d994b194
KleprlikJan <kleprlikjan@gmail.com>
2021-02-16 23:59:29 +0100

    Refactoring AudioRecording

---
+++ BP/BP.Shared/AudioRecorder/Recorder.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+#if NETFX_CORE
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Media.Playback;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+#endif
+
+#if __ANDROID__
+using Android.Media;
+using System.IO;
+#endif
+
+
+#region ANDROID
+#if __ANDROID__
+#endif
+#endregion
+
+
+namespace BP.Shared.AudioRecorder
+{
+	public class Recorder
+	{
+		private bool isRecording;
+		#region UWP
+#if NETFX_CORE
+		MediaCapture audioCapture;
+		InMemoryRandomAccessStream buffer;
+		string filename;
+		string audioFile = "recording.wav";
+#endif
+		#endregion
+		#region ANDROID
+#if __ANDROID__
+		private string filePath;
+		private MediaRecorder recorder;
+		private MediaPlayer player;
+
+		public Recorder()
+		{
+			filePath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData), "recording.mp4");
+		}
+#endif
+		#endregion
+
+
+		public async Task<bool> StartRecording()
+		{
+			if (isRecording)
+			{
+				//already recording
+				return false;
+			}
+			else
+			{
+				#region UWP
+#if NETFX_CORE
+				await setupRecording();
+				await audioCapture.StartRecordToStreamAsync(MediaEncodingProfile.CreateWav(AudioEncodingQuality.Low), buffer);
+				isRecording = true;
+				return true;
+#endif
+				#endregion
+				#region ANDROID
+#if __ANDROID__
+				try
+				{
+					if (System.IO.File.Exists(filePath))
+						System.IO.File.Delete(filePath);
+								
+					//setup recorder
+					if (recorder == null)
+					{
+						recorder = new MediaRecorder(); // Initial state.
+					}
+					else
+					{
+						recorder.Reset();
+					}
+
+					//prepare recording settings
+					recorder.SetAudioSource(AudioSource.Mic);
+					recorder.SetOutputFormat(OutputFormat.Mpeg4);
+					recorder.SetAudioChannels(1);
+					recorder.SetAudioSamplingRate(11025);
+					recorder.SetAudioEncodingBitRate(44000);
+					recorder.SetAudioEncoder(AudioEncoder.HeAac);
+
+					//setup 
+					recorder.SetOutputFile(filePath);
+					recorder.Prepare();
+
+					//record
+					recorder.Start();
+
+
+					isRecording = true;
+					return true;
+				}
+				catch (Exception ex)
+				{
+					Console.Out.WriteLine(ex.StackTrace);
+					isRecording = false;
+					return false;
+				}
+#endif
+				#endregion
+			}
+		}
+
+		public async Task<bool> StopRecording()
+		{
+			if (!isRecording)
+			{
+				//not recording yet
+				return false;
+			}
+			else
+			{
+				#region UWP
+#if NETFX_CORE
+				//to disalbe fast RUN-STOP sequences
+				Thread.Sleep(300);
+				await audioCapture.StopRecordAsync();
+#endif
+				#endregion
+				#region ANDROID
+#if __ANDROID__
+				recorder.Stop();
+				recorder.Release();
+#endif
+				#endregion
+				isRecording = false;
+				return true;
+			}
+
+		}
+		
+		
+		//replay functions
+		#region UWP
+#if NETFX_CORE
+
+		public async Task<bool> ReplayRecordingUWP(CoreDispatcher UIDispatcher)
+		{
+			//do nothign without buffer
+			if (buffer == null)
+				return false;
+
+
+			MediaElement playback = new MediaElement();
+			IRandomAccessStream audioBuffer = buffer.CloneStream();
+
+			if (audioBuffer == null)
+				throw new ArgumentNullException("buffer");
+
+			StorageFolder recordingFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+
+			//delete if not empty
+			if (!string.IsNullOrEmpty(filename))
+			{
+				StorageFile original = await recordingFolder.GetFileAsync(filename);
+				await original.DeleteAsync();
+			}
+
+			//replay asynch
+			await UIDispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+			{
+				StorageFile recordingFile = await recordingFolder.CreateFileAsync(audioFile, CreationCollisionOption.GenerateUniqueName);
+				filename = recordingFile.Name;
+
+				//save buffer into file
+				using (IRandomAccessStream fs = await recordingFile.OpenAsync(FileAccessMode.ReadWrite))
+				{
+					await RandomAccessStream.CopyAndCloseAsync(audioBuffer.GetInputStreamAt(0), fs.GetOutputStreamAt(0));
+					await audioBuffer.FlushAsync();
+					audioBuffer.Dispose();
+				}
+
+				//replay actuall recording
+				IRandomAccessStream replayStream = await recordingFile.OpenAsync(FileAccessMode.Read);
+				playback.SetSource(replayStream, recordingFile.FileType);
+				playback.Play();
+			});
+
+			return true;
+		}
+#endif
+		#endregion
+		#region ANDROID
+#if __ANDROID__
+		public async Task<bool> ReplayRecordingANDROID()
+		{
+			//dont do anything if nothing was recorded
+			if (filePath == null)
+				return false;
+
+			//reset player
+			if (player == null)
+			{
+				player = new Android.Media.MediaPlayer();
+			}
+			else
+			{
+				player.Reset();
+			}
+
+			//replay the song
+			player.SetDataSource(filePath);
+			player.Prepare();
+			player.Start();
+			return true;
+		}
+#endif
+		#endregion
+
+
+		// helper functons
+		#region UWP - helper functions
+#if NETFX_CORE
+
+		private async Task<bool> setupRecording()
+		{
+			if (buffer != null)
+				buffer.Dispose();
+			if (audioCapture != null)
+				audioCapture.Dispose();
+
+			buffer = new InMemoryRandomAccessStream();
+
+			try
+			{
+				MediaCaptureInitializationSettings settings = new MediaCaptureInitializationSettings
+				{
+					StreamingCaptureMode = StreamingCaptureMode.Audio,
+				};
+
+				audioCapture = new MediaCapture();
+				//initialize setup
+				await audioCapture.InitializeAsync(settings);
+				//exceed limitation handler
+				audioCapture.RecordLimitationExceeded += async (MediaCapture sender) =>
+				{
+					await audioCapture.StopRecordAsync();
+					isRecording = false;
+					throw new Exception("Record limitation exceeded.");
+				};
+				//on fail handler
+				audioCapture.Failed += (MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs) =>
+				{
+					isRecording = false;
+					throw new Exception(string.Format("Audio Capture failed. CODE: {0}. {1}", errorEventArgs.Code, errorEventArgs.Message));
+				};
+			}
+			catch(Exception e)
+			{
+				throw;
+			}
+			return true;
+
+		}
+
+#endif
+		#endregion
+
+		#region ANDROID - helper functions
+#if __ANDROID__
+		public async Task<bool> getMicPermission()
+		{
+			CancellationTokenSource source = new CancellationTokenSource();
+			CancellationToken token = source.Token;
+			return await Windows.Extensions.PermissionsHelper.TryGetPermission(token, Android.Manifest.Permission.RecordAudio);
+		}
+#endif
+#endregion
+	}
+}
--- BP/BP.Shared/Views/MainPage.xaml.cs
+++ BP/BP.Shared/Views/MainPage.xaml.cs
@@ -34,7 +34,6 @@ using Android.Content.PM;
 using Uno.Foundation;
 #endif
 
-
 namespace BP
 {
     /// <summary>
@@ -42,93 +41,8 @@ namespace BP
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        bool record;
-#region UWP
-#if NETFX_CORE
-        MediaCapture capture;
-        InMemoryRandomAccessStream buffer;
-        string filename;
-        string audioFile = "UWP_recording.wav";
-        private async Task<bool> RecordProcess()
-        {
-            if (buffer != null)
-            {
-                buffer.Dispose();
-            }
-            buffer = new InMemoryRandomAccessStream();
-            if (capture != null)
-            {
-                capture.Dispose();
-            }
-            try
-            {
-                MediaCaptureInitializationSettings settings = new MediaCaptureInitializationSettings
-                {
-                    StreamingCaptureMode = StreamingCaptureMode.Audio
-                };
-                capture = new MediaCapture();
-                await capture.InitializeAsync(settings);
-                capture.RecordLimitationExceeded += (MediaCapture sender) =>
-                {
-                    //Stop  
-                    // await capture.StopRecordAsync();  
-                    record = false;
-                    throw new Exception("Record Limitation Exceeded ");
-                };
-                capture.Failed += (MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs) =>
-                {
-                    record = false;
-                    throw new Exception(string.Format("Code: {0}. {1}", errorEventArgs.Code, errorEventArgs.Message));
-                };
-            }
-            catch (Exception ex)
-            {
-                if (ex.InnerException != null && ex.InnerException.GetType() == typeof(UnauthorizedAccessException))
-                {
-                    throw ex.InnerException;
-                }
-                throw;
-            }
-            return true;
-        }
-        public async Task PlayRecordedAudio(CoreDispatcher UiDispatcher)
-        {
-            MediaElement playback = new MediaElement();
-            IRandomAccessStream audio = buffer.CloneStream();
 
-            if (audio == null)
-                throw new ArgumentNullException("buffer");
-            StorageFolder storageFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
-            if (!string.IsNullOrEmpty(filename))
-            {
-                StorageFile original = await storageFolder.GetFileAsync(filename);
-                await original.DeleteAsync();
-            }
-            await UiDispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
-            {
-                StorageFile storageFile = await storageFolder.CreateFileAsync(audioFile, CreationCollisionOption.GenerateUniqueName);
-                filename = storageFile.Name;
-                using (IRandomAccessStream fileStream = await storageFile.OpenAsync(FileAccessMode.ReadWrite))
-                {
-                    await RandomAccessStream.CopyAndCloseAsync(audio.GetInputStreamAt(0), fileStream.GetOutputStreamAt(0));
-                    await audio.FlushAsync();
-                    audio.Dispose();
-                }
-                IRandomAccessStream stream = await storageFile.OpenAsync(FileAccessMode.Read);
-                playback.SetSource(stream, storageFile.FileType);
-                playback.Play();
-            });
-        }
-#endif
-#endregion
-#region ANDROID
-#if __ANDROID__
-        bool external = false;
-        private string filePath;
-        private MediaRecorder recorder;
-        private Android.Media.MediaPlayer player;
-#endif
-#endregion
+		Shared.AudioRecorder.Recorder recorder;
 #region WASM
 
 #endregion
@@ -136,19 +50,8 @@ namespace BP
 		public MainPage()
         {
             this.InitializeComponent();
+			recorder = new Shared.AudioRecorder.Recorder();
             textBlk.Text = "I am ready";
-#region ANDROID
-#if __ANDROID__
-            if (external)
-			{
-                filePath = Path.Combine(Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryMusic).AbsolutePath, "recording.mp4");
-            }
-            else
-			{
-                filePath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData), "recording.mp4");
-            }
-#endif
-#endregion
 		}
 
 
@@ -156,89 +59,10 @@ namespace BP
 
 		private async void recordBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (record)
-            {
-                //already recored process  
-            }
-            else
-            {
-#region UWP
-#if NETFX_CORE
-				await RecordProcess();
-                await capture.StartRecordToStreamAsync(MediaEncodingProfile.CreateWav(AudioEncodingQuality.Low), buffer);
-                if (record)
-                {
-                    throw new InvalidOperationException();
-                }
-
-#endif
-#endregion
-#region ANDROID
-#if __ANDROID__
-
-                //check for permission
-                CancellationTokenSource source = new CancellationTokenSource();
-                CancellationToken token = source.Token;
-
-                
-                Console.Out.WriteLine("[DEBUG] granting access to microphone");
-                bool canAccessMicrophone = await Windows.Extensions.PermissionsHelper.TryGetPermission(token, Android.Manifest.Permission.RecordAudio);
-                if (!canAccessMicrophone)
-				{
-                    Console.Out.WriteLine("[DEBUG] cannot access microphone");
-                    return;
-				}
-
-				if (external)
-				{
-                    bool canAccessStorageW = await Windows.Extensions.PermissionsHelper.TryGetPermission(token, Android.Manifest.Permission.WriteExternalStorage);
-                    if (!canAccessStorageW)
-                        return;                
-                    bool canAccessStorageR = await Windows.Extensions.PermissionsHelper.TryGetPermission(token, Android.Manifest.Permission.ReadExternalStorage);
-                    if (!canAccessStorageR)
-                        return;
-				}
-                
-                try
-                {
-                    
-		            if (System.IO.File.Exists(filePath))
-		            {
-                        Console.Out.WriteLine("[DEBUG] deleting existing file");
-			            System.IO.File.Delete(filePath);
-		            }
-                    
-                    if (recorder == null)
-                    {
-                        recorder = new MediaRecorder(); // Initial state.
-                    }
-                    else
-                    {
-                        recorder.Reset();
-                    }
-
-                    Console.Out.WriteLine("[DEBUG] setting up recording options");
-                    recorder.SetAudioSource(AudioSource.Mic);
-                    recorder.SetOutputFormat(OutputFormat.Mpeg4);
-                    recorder.SetAudioChannels(1);
-                    recorder.SetAudioSamplingRate(11025);
-                    recorder.SetAudioEncodingBitRate(44000);
-                    recorder.SetAudioEncoder(AudioEncoder.HeAac);
-                    Console.Out.WriteLine("[DEBUG] seting up outputfile");
-                    Console.Out.WriteLine("[DEBUG]" + filePath);
-                    recorder.SetOutputFile(filePath);
-                    recorder.Prepare();
-                    Console.Out.WriteLine("[DEBUG] starting to listen");
-                    recorder.Start();
-
-
-                }
-                catch (Exception ex)
-                {
-                    Console.Out.WriteLine("[DEBUG]" + ex.StackTrace);
-                }
-#endif
-#endregion
+			if (await recorder.StartRecording())
+			{
+				textBlk.Text = "Called library and am recording...";
+			}
 #region WASM
 #if __WASM__
 
@@ -271,69 +95,37 @@ namespace BP
     input.click(); ");
 #endif
 #endregion
+            
+		}
 
-                record = true;
-                textBlk.Text = "Recording ...";
-
-            }
-        }
-
-        private async void stopBtn_Click(object sender, RoutedEventArgs e)
+		private async void stopBtn_Click(object sender, RoutedEventArgs e)
         {
-#region UWP
-#if NETFX_CORE
-			Thread.Sleep(300);
-            await capture.StopRecordAsync();
-#endif
-#endregion
-#region ANDROID
-
-#if __ANDROID__
-            Console.Out.WriteLine("[DEBUG] STOP recording clicked.");
-            if (recorder != null)
-            {
-                Console.Out.WriteLine("[DEBUG] Stopping the recording.");
-                recorder.Stop();
-                recorder.Release();
-                recorder = null;
-                Console.Out.WriteLine("[DEBUG] Stopped the recording.");
-            }
-#endif
-
-#endregion
-            record = false;
-            textBlk.Text = "Stopped recording.";
+			if (await recorder.StopRecording())
+			{
+				textBlk.Text = "Stopped recording from lib.";
+			}
         }
 
         private async void playBtn_Click(object sender, RoutedEventArgs e)
         {
-            Console.Out.WriteLine("[DEBUG] PLAY clicked.");
-#region UWP
+		#region UWP
 #if NETFX_CORE
-            await PlayRecordedAudio(Dispatcher);
+			if (await recorder.ReplayRecordingUWP(Dispatcher))
+			{
+				textBlk.Text = "Replaying recorded sound.";				
+			}
 #endif
-#endregion
-#region ANDROID
-
+		#endregion
+		#region ANDROID
 #if __ANDROID__
-
-            if (player == null)
-            {
-                player = new Android.Media.MediaPlayer();
-            }
-            else
-            {
-                player.Reset();
-            }
-            Console.Out.WriteLine("[DEBUG] Playing the recording.");
-            player.SetDataSource(filePath);
-            player.Prepare();
-            player.Start();
+		if (await recorder.ReplayRecordingANDROID())
+		{
+			textBlk.Text = "Replaying recorded sound.";
+		}
 #endif
-#endregion
-            textBlk.Text = "Replaying captured sound.";
+		#endregion
         }
-
+	/*
         public static void SelectFile(string imageAsDataUrl) => FileSelectedEvent?.Invoke(null, new FileSelectedEventHandlerArgs(imageAsDataUrl));
 
         private void OnFileSelectedEvent(object sender, FileSelectedEventHandlerArgs e)
@@ -362,6 +154,6 @@ namespace BP
             public FileSelectedEventHandlerArgs(string fileAsDataUrl) => FileAsDataUrl = fileAsDataUrl;
 
         }
-
+	*/
     }
 }
