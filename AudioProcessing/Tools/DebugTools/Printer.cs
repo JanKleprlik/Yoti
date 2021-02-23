104bb5c218c391472b180103a40137351052f39f
KleprlikJan <kleprlikjan@gmail.com>
2021-02-23 19:01:48 +0100

    Add DebugTools to git

---
+++ AudioProcessing/Tools/DebugTools/Printer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AudioProcessing.Tools
+{
+	public static class Printer
+	{
+		public static void Print(byte[] data)
+		{
+			int limit = Math.Min(20, data.Length);
+			for (int i = 0; i < limit; i++)
+			{
+				System.Diagnostics.Debug.WriteLine(data[i]);
+			}
+		}
+
+		public static void PrintShortAsBytes(short[] data)
+		{
+			int limit = Math.Min(50, data.Length);
+			for (int i = 0; i < limit; i++)
+			{
+				byte first = (byte)(data[i] << 4);
+				byte second = (byte)data[i];
+				System.Diagnostics.Debug.Write(BitConverter.ToString(new[] { first }) + " ");
+				System.Diagnostics.Debug.Write(BitConverter.ToString(new[] { second }) + " ");
+			}
+		}
+	}
+}
