75d39b0421caf614b943c6bbd82ab203b7232aed
KleprlikJan <kleprlikjan@gmail.com>
2021-02-26 13:21:48 +0100

    Add AudioRecognizer API

--- AudioProcessing/Recognizer/AudioRecognizer.cs
+++ AudioProcessing/Recognizer/AudioRecognizer.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace AudioProcessing.Recognizer
 {
 	public class AudioRecognizer
 	{
+		public AudioRecognizer()
+		{
+			//TODO: setup database SQL_lite 3 ???
+		}
+
+		public void AddNewSong()
+		{
+
+		}
+
+		public string RecognizeSong()
+		{
+			return "Not recognized";
+		}
+
+		public void ListSongs(TextWriter output)
+		{
+			
+		}
 	}
 }
