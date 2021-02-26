1f19342810175822b7287662823397c2ecee7552
KleprlikJan <kleprlikjan@gmail.com>
2021-02-26 19:49:55 +0100

    Add basic Database layout

---
+++ AudioProcessing/Database/Types.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace AudioProcessing.Database
+{
+	[Table("Songs")]
+	public class Song
+	{
+		[PrimaryKey, AutoIncrement, Column("id")]
+		public int ID { get; set; }
+
+		[Column("name"), MaxLength(20)]
+		public string Name { get; set; }
+
+		[Column("author"), MaxLength(20)]
+		public string Author { get; set; }
+	}
+
+	[Serializable]
+	public class Fingerprint
+	{
+		[PrimaryKey, AutoIncrement, Column("id")]
+		public int ID { get; set; }
+
+		[TextBlob("TimeFrequencyPointsBlobbed")]
+		public List<TimeFrequencyPoint> TimeFrequencyPoints { get; set; }
+		public string TimeFrequencyPointsBlobbed { get; set; }
+	}
+	
+	public struct TimeFrequencyPoint
+	{
+		public uint Time { get; set; }
+		public uint Frequency { get; set; }
+	}
+}
