2ccfe1baf813be73360d181d0b77cab0dda24847
KleprlikJan <kleprlikjan@gmail.com>
2021-02-28 16:04:21 +0100

    Add Visualizer Modes base

---
+++ Visualizer/MusicModes/AbstractMode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using SFML.Audio;
+using SFML.Graphics;
+
+namespace Visualizer.MusicModes
+{
+	abstract class AbstractMode : IVisualiserMode
+	{
+		#region Constructors
+		protected AbstractMode(SoundBuffer sb)
+		{
+			Song = new Sound(sb);
+			SampleRate = sb.SampleRate;
+			SampleCount = (uint)sb.Samples.Length;
+			BufferSize = 4096;
+			Samples = sb.Samples;
+			if (sb.ChannelCount > 2)
+			{
+				throw new ArgumentException($"Too many channels: {sb.ChannelCount}");
+			}
+			ChannelCount = sb.ChannelCount;
+		}
+
+		#endregion
+
+		#region Properties
+		protected Sound Song { get; set; }
+		protected short[] Samples { get; }
+		protected uint SampleRate { get; }
+		protected uint SampleCount { get; }
+		protected uint BufferSize { get; }
+		protected uint ChannelCount { get; }
+		#endregion
+
+		#region  API
+		public abstract void Draw(RenderWindow window);
+		public abstract void Update();
+
+		public virtual void Quit()
+		{
+			if (Song != null)
+			{
+				Song.Stop();
+				Song.Dispose();
+			}
+		}
+
+		#endregion
+
+	}
+}
---
+++ Visualizer/MusicModes/IVisualizerMode.cs
@@ -0,0 +1,11 @@
+﻿using SFML.Graphics;
+
+namespace Visualizer.MusicModes
+{
+	interface IVisualiserMode
+	{
+		void Draw(RenderWindow window);
+		void Update();
+		void Quit();
+	}
+}
