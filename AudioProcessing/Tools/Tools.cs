7507024cd9b34ab477ae35b1086c006cb2e7fa21
KleprlikJan <kleprlikjan@gmail.com>
2021-03-02 18:03:37 +0100

    Add FFT

--- AudioProcessing/Tools/Tools.cs
+++ AudioProcessing/Tools/Tools.cs
@@ -10,5 +10,58 @@ namespace AudioProcessing.Tools
 		{
 			return (short)((left + right) / 2);
 		}
+
+		/// <summary>
+		/// Swap two values
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="first">Value 1</param>
+		/// <param name="second">Value 2</param>
+		/// <returns>Value 2</returns>
+		public static T Swap<T>(this T first, ref T second)
+		{
+			T tmp = second;
+			second = first;
+			return tmp;
+		}
+
+		/// <summary>
+		/// Returns <c>_base</c> to the power of <c>exp</c>
+		/// </summary>
+		/// <param name="_base"></param>
+		/// <param name="exp"></param>
+		/// <returns></returns>
+		public static int ToPowOf(this int _base, int exp)
+		{
+			int res = _base;
+			for (int i = 0; i < exp; i++)
+			{
+				res *= _base;
+			}
+
+			return res;
+		}
+
+		/// <summary>
+		/// Returns second power of <c>_base</c>
+		/// </summary>
+		/// <param name="_base"></param>
+		/// <returns></returns>
+		public static double Pow2(this double _base)
+		{
+			return _base * _base;
+		}
+
+		public static double GetComplexAbs(double real, double img)
+		{
+			return Math.Sqrt(real.Pow2() + img.Pow2());
+		}
+
+		public static bool IsPowOfTwo(int n)
+		{
+			if ((n & (n - 1)) != 0)
+				return false;
+			return true;
+		}
 	}
 }
