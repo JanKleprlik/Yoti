ed8e1a244370324dd7e0bb0f1faf76416e059547
KleprlikJan <kleprlikjan@gmail.com>
2021-03-21 08:29:40 +0100

    Add MainPage platform specific

---
+++ BP/BP.Shared/Views/MainPageANDROID.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+
+namespace BP.Shared.Views
+{
+	public sealed partial class MainPage : Page
+	{
+	}
+}
---
+++ BP/BP.Shared/Views/MainPageUWP.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+
+namespace BP.Shared.Views
+{
+	public sealed partial class MainPage : Page
+	{
+#if NETFX_CORE
+
+
+
+#endif
+
+	}
+}
