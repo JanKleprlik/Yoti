6fed8e55a00b0ae08367233cbd2d14699bca40af
KleprlikJan <kleprlikjan@gmail.com>
2021-03-27 23:50:13 +0100

    Add Settings View Model

---
+++ BP/BP.Shared/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,62 @@
+﻿using BP.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BP.Shared.ViewModels
+{
+	public class SettingsViewModel : BaseViewModel
+	{
+		public SettingsViewModel(Settings settings = null) => Settings = settings ?? new Settings();
+		private Settings settings;
+		public Settings Settings
+		{
+			get => settings;
+			set
+			{
+				settings = value;
+				OnPropertyChanged(string.Empty);
+			}
+		}
+
+		public bool ConstQAlg
+		{
+			get => Settings.ConstQAlgorithm;
+			set
+			{
+				Settings.ConstQAlgorithm = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public bool DetailedInfo
+		{
+			get => Settings.DetailedInfo;
+			set
+			{
+				Settings.DetailedInfo = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public int RecordingLength
+		{
+			get => Settings.RecordingLength;
+			set
+			{
+				Settings.RecordingLength = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public bool UseMicrophone
+		{
+			get => Settings.UseMicrophone;
+			set
+			{
+				Settings.UseMicrophone = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+}
