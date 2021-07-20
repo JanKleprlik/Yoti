ba1b4398ced0f9e39044557b9c57651437d636ea
KleprlikJan <kleprlikjan@gmail.com>
2021-07-21 01:08:44 +0200

    Update Main

--- Visualizer/Program.cs
+++ Visualizer/Program.cs
@@ -7,37 +7,28 @@ namespace Visualizer
 	{
 		static void Main(string[] args)
 		{
+			// Write names of the songs you want to visualize
 			string[] files = new string[] {
 				"Home.wav",
-				"home_3.wav",
-				"home_5.wav",
-				"home_8.wav",
-				"home_5b.wav",
-				"Havana.wav",
-				"havana_3.wav",
-				"havana_5.wav",
-				"havana_8.wav",
 			};
-			//AudioRecognitionLibrary.Recognizer.AudioRecognizer recognizer = new AudioRecognitionLibrary.Recognizer.AudioRecognizer();
-			//Console.WriteLine($"BPMLowFreq: {AudioRecognitionLibrary.Recognizer.AudioRecognizer.Parameters.BPMLowFreq}\n" +
-			//					$"BPMHighFreq: {AudioRecognitionLibrary.Recognizer.AudioRecognizer.Parameters.BPMHighFreq}\n" +
-			//					$"PartsPerSecond: {AudioRecognitionLibrary.Recognizer.AudioRecognizer.Parameters.PartsPerSecond}\n" +
-			//					$"BPMLowLimit: {AudioRecognitionLibrary.Recognizer.AudioRecognizer.Parameters.BPMLowLimit}\n" +
-			//					$"BPMHighLimit: {AudioRecognitionLibrary.Recognizer.AudioRecognizer.Parameters.BPMHighLimit}\n" +
-			//					$"PeakNeighbourRange: {AudioRecognitionLibrary.Recognizer.AudioRecognizer.Parameters.PeakNeighbourRange}");
-			//foreach (var file in files)
-			//{
 
-			//	var audio = Recorder.GetAudio($"Resources/Songs/{file}");
-			//	Console.WriteLine($"FILE: {file}");
-			//	Console.WriteLine(recognizer.GetBPM(audio, true));
-			//}
 
-			string file = "Avicii.wav";
-			var audio = Recorder.GetAudio($"Resources/Songs/{file}");
-			AudioProcessor.ConvertToMono(audio);
-			var window = new global::Visualizer.Visualizer(audio.Data, audio.Channels, audio.SampleRate, VisualisationModes.Frequencies);
-			window.Run();
+			AudioRecognitionLibrary.Recognizer.AudioRecognizer recognizer = new AudioRecognitionLibrary.Recognizer.AudioRecognizer();
+
+			foreach (var file in files)
+			{
+				// Compute BPM and write result into the console
+				var audio = Recorder.GetAudio($"Resources/Songs/{file}");
+				Console.WriteLine($"FILE: {file}");
+				Console.WriteLine(recognizer.GetBPM(audio, true));
+				
+				
+				// Run visualisation
+				AudioProcessor.ConvertToMono(audio);
+				// Change last parameter to see other visualisations
+				var window = new global::Visualizer.Visualizer(audio.Data, audio.Channels, audio.SampleRate, VisualisationModes.Frequencies);
+				window.Run();
+			}
 		}
 	}
 }
